6abbfbff1dc687483d1dee93a8b00ec199598e12
Alex Suraci <suraci.alex@gmail.com>
2015-10-08 19:54:13 -0700

    switch to Iosevka; easier to get to

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,7 +1,18 @@
 import XMonad
 
+import qualified Data.Map as M
+
 main = xmonad $ defaultConfig
     { borderWidth        = 2
     , terminal           = "termite"
     , normalBorderColor  = "#393939"
-    , focusedBorderColor = "#ca674a" }
+    , focusedBorderColor = "#ca674a"
+    , keys               = myKeys
+    }
+
+myKeys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
+myKeys c@(XConfig { modMask = modMask }) =
+  M.insert
+    (modMask, xK_p)
+    (spawn "dmenu_run -fn 'Iosevka:14'")
+    ((keys defaultConfig) c)
