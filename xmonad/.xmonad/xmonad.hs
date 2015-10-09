ba0468476e656268f4c496db606b53dba26c7077
Alex Suraci <asuraci@pivotal.io>
2015-10-09 16:46:28 -0700

    add xmobar config to xmonad

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,13 +1,25 @@
 import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Util.Run (spawnPipe)
+import XMonad.Hooks.ManageDocks
+import System.IO (hPutStrLn)
 
 import qualified Data.Map as M
 
-main = xmonad $ defaultConfig
-    { borderWidth        = 2
+main = do
+  xmproc <- spawnPipe "xmobar"
+
+  xmonad $ defaultConfig
+    { manageHook = manageDocks <+> manageHook defaultConfig
+    , layoutHook = avoidStruts $ layoutHook defaultConfig
+    , logHook = dynamicLogWithPP xmobarPP
+                  { ppOutput = hPutStrLn xmproc
+                  , ppTitle = xmobarColor "green" "" . shorten 50
+                  }
+    , borderWidth        = 2
     , terminal           = "termite"
     , normalBorderColor  = "#393939"
     , focusedBorderColor = "#ca674a"
-    , keys               = myKeys
     }
 
 myKeys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
