4591e6928dbe509e6fdd518fd50371f57662f241
Alex Suraci <suraci.alex@gmail.com>
2015-10-09 19:26:27 -0700

    don't mention specific user

--- xorg-laptop/.screenlayout/hidpi-lowdpi.sh
+++ xorg-laptop/.screenlayout/hidpi-lowdpi.sh
@@ -11,6 +11,8 @@ lowdpiy=1440
 scale=2
 
 xrandr \
-  --output eDP1 --mode ${hidpix}x${hidpiy} --pos 0x0 --rotate normal \
-  --output DP1 --mode ${lowdpix}x${lowdpiy} --right-of eDP1 --rotate normal \
-  --scale ${scale}x${scale} --panning "$(expr $lowdpix \* $scale)x$(expr $lowdpiy \* $scale)+${hidpix}+0"
+  --output eDP1 --mode ${hidpix}x${hidpiy} \
+  --output  DP1 --mode ${lowdpix}x${lowdpiy} \
+    --right-of eDP1 --primary \
+    --scale ${scale}x${scale} \
+    --panning "$(expr $lowdpix \* $scale)x$(expr $lowdpiy \* $scale)+${hidpix}+0"
