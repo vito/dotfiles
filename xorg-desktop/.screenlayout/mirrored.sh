f6df5cb25b14e8ad983a79e67a0f06ce3e3e9f75
Alex Suraci <suraci.alex@gmail.com>
2015-10-10 09:40:40 -0700

    explicitly turn other inputs off

--- xorg-desktop/.screenlayout/mirrored.sh
+++ xorg-desktop/.screenlayout/mirrored.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 xrandr \
-  --output DP-3 --mode 2560x1440 --pos 0x0 --rotate normal \
-  --output DP-2 --mode 2560x1440 --pos 0x0 --rotate normal
+  --output DP-2 --mode 2560x1440 --pos 0x0 --rotate normal \
+  --output DP-3 --mode 2560x1440 --pos 0x0 --rotate normal
--- xorg-laptop/.screenlayout/hidpi-lowdpi.sh
+++ xorg-laptop/.screenlayout/hidpi-lowdpi.sh
@@ -11,8 +11,12 @@ lowdpiy=1440
 scale=2
 
 xrandr \
-  --output eDP1 --mode ${hidpix}x${hidpiy} \
-  --output  DP1 --mode ${lowdpix}x${lowdpiy} \
+  --output VIRTUAL1 --off
+  --output DP1 --mode ${lowdpix}x${lowdpiy} \
     --right-of eDP1 --primary \
     --scale ${scale}x${scale} \
-    --panning "$(expr $lowdpix \* $scale)x$(expr $lowdpiy \* $scale)+${hidpix}+0"
+    --panning "$(expr $lowdpix \* $scale)x$(expr $lowdpiy \* $scale)+${hidpix}+0" \
+  --output DP2 --off \
+  --output eDP1 --mode ${hidpix}x${hidpiy} \
+  --output HDMI1 --off \
+  --output HDMI2 --off
