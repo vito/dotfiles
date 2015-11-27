4c879b021c921b7192f80fd816753d8fc67d2027
Alex Suraci <suraci.alex@gmail.com>
2015-11-26 21:23:19 -0800

    configure better colors

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -20,3 +20,27 @@ set -x PATH $HOME/bin $PATH
 if which keychain >/dev/null
   eval (keychain --eval --quiet)
 end
+
+set fish_color_autosuggestion white
+set fish_color_command blue
+set fish_color_comment yellow
+set fish_color_cwd blue
+set fish_color_cwd_root red
+set fish_color_end magenta
+set fish_color_error red
+set fish_color_escape cyan
+set fish_color_history_current cyan
+set fish_color_match cyan
+set fish_color_normal normal
+set fish_color_operator cyan
+set fish_color_param cyan
+set fish_color_quote green
+set fish_color_redirection red
+set fish_color_search_match \x2d\x2dbackground\x3dpurple
+set fish_color_selection \x2d\x2dbackground\x3dpurple
+set fish_color_valid_path \x2d\x2dunderline
+set fish_key_bindings fish_default_key_bindings
+set fish_pager_color_completion normal
+set fish_pager_color_description 555\x1eyellow
+set fish_pager_color_prefix cyan
+set fish_pager_color_progress cyan
