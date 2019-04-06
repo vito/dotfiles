19328d728e4f9f72c06b61d22a6ed0d969e4f9b7
Alex Suraci <suraci.alex@gmail.com>
2019-04-06 14:00:14 -0400

    add fish color config
    i have no idea why i have to do this, it "just worked" on my pixelbook
    :|
    
    Signed-off-by: Alex Suraci <suraci.alex@gmail.com>

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -16,3 +16,30 @@ for path in $HOME/bin $HOME/go/bin /usr/lib/go-1.12/bin
     set -x PATH $path $PATH
   end
 end
+
+set fish_color_autosuggestion 555\x1ebrblack
+set fish_color_command \x2d\x2dbold
+set fish_color_comment red
+set fish_color_cwd green
+set fish_color_cwd_root red
+set fish_color_end brmagenta
+set fish_color_error brred
+set fish_color_escape bryellow\x1e\x2d\x2dbold
+set fish_color_history_current \x2d\x2dbold
+set fish_color_host normal
+set fish_color_match \x2d\x2dbackground\x3dbrblue
+set fish_color_normal normal
+set fish_color_operator bryellow
+set fish_color_param cyan
+set fish_color_quote yellow
+set fish_color_redirection brblue
+set fish_color_search_match bryellow\x1e\x2d\x2dbackground\x3dbrblack
+set fish_color_selection white\x1e\x2d\x2dbold\x1e\x2d\x2dbackground\x3dbrblack
+set fish_color_user brgreen
+set fish_color_valid_path \x2d\x2dunderline
+set fish_greeting \x1d
+set fish_key_bindings fish_default_key_bindings
+set fish_pager_color_completion \x1d
+set fish_pager_color_description B3A06D\x1eyellow
+set fish_pager_color_prefix white\x1e\x2d\x2dbold\x1e\x2d\x2dunderline
+set fish_pager_color_progress brwhite\x1e\x2d\x2dbackground\x3dcyan
