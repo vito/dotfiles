5da2b6d37bbf1f02c3c67a6e27e01fdc7666b778
Alex Suraci <suraci.alex@gmail.com>
2021-10-28 10:49:57 -0400

    fish: newer base16 (+ patched rose-pine), async

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -36,3 +36,12 @@ install_path /opt/google-cloud-sdk/bin
 if test -d /usr/lib/postgresql
   install_path /usr/lib/postgresql/*/bin
 end
+
+# faster git prompt
+set -g async_prompt_functions _pure_prompt_git
+
+# setting loading indicator for _pure_prompt_git doesn't work, so this approximates it
+function fish_prompt_loading_indicator
+  echo (_pure_prompt_current_folder 0)' '(set_color brblack)'…'(set_color normal)
+  echo (_pure_prompt_symbol 0)' '
+end
