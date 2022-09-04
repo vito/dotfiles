bd785db76774ab34091e662613768a21265885a0
Alex Suraci <suraci.alex@gmail.com>
2022-09-04 15:54:44 -0400

    lots of progress on a sway setup

--- home.nix
+++ home.nix
@@ -1,5 +1,12 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
+let
+  ws1 = "1: werk";
+  ws2 = "2: web";
+  ws3 = "3: chat";
+  ws4 = "4: notes";
+  ws5 = "5: music";
+in
 {
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
@@ -18,6 +25,7 @@
     _1password-gui
     discord
     firefox-wayland
+    signal-desktop
 
     # misc shell utils
     ripgrep
@@ -26,74 +34,8 @@
 
     # language server providers
     rnix-lsp
+    nixpkgs-fmt
   ];
-
-  programs.wezterm.enable = true;
-  programs.wezterm.extraConfig = ''
-    local wezterm = require 'wezterm'
-
-    return {
-      font = wezterm.font "Iosevka Term",
-      color_scheme = "Rosé Pine (base16)",
-      keys = {
-        {
-          key = '+',
-          mods = 'ALT|SHIFT',
-          action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
-        },
-        {
-          key = '_',
-          mods = 'ALT|SHIFT',
-          action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
-        },
-        {
-          key = 'Enter',
-          mods = 'ALT|SHIFT',
-          action = wezterm.action.TogglePaneZoomState,
-        },
-        {
-          key = 'LeftArrow',
-          mods = 'ALT',
-          action = wezterm.action.ActivatePaneDirection 'Left',
-        },
-        {
-          key = 'RightArrow',
-          mods = 'ALT',
-          action = wezterm.action.ActivatePaneDirection 'Right',
-        },
-        {
-          key = 'UpArrow',
-          mods = 'ALT',
-          action = wezterm.action.ActivatePaneDirection 'Up',
-        },
-        {
-          key = 'DownArrow',
-          mods = 'ALT',
-          action = wezterm.action.ActivatePaneDirection 'Down',
-        },
-        {
-          key = 'LeftArrow',
-          mods = 'ALT|SHIFT',
-          action = wezterm.action.AdjustPaneSize { 'Left', 5},
-        },
-        {
-          key = 'RightArrow',
-          mods = 'ALT|SHIFT',
-          action = wezterm.action.AdjustPaneSize { 'Right', 5},
-        },
-        {
-          key = 'UpArrow',
-          mods = 'ALT|SHIFT',
-          action = wezterm.action.AdjustPaneSize { 'Up', 5 },
-        },
-        {
-          key = 'DownArrow',
-          mods = 'ALT|SHIFT',
-          action = wezterm.action.AdjustPaneSize { 'Down', 5 },
-        },
-      },
-    }
-  '';
 
   programs.git = {
     enable = true;
@@ -104,7 +46,8 @@
       co = "checkout";
       st = "status";
     };
-    delta = { # fancy syntax highlighting
+    delta = {
+      # fancy syntax highlighting
       enable = true;
     };
     extraConfig = {
@@ -169,6 +112,184 @@
   xdg.configFile."nvim" = {
     source = ~/src/dotfiles/nvim/.config/nvim;
     recursive = true;
+  };
+
+  wayland.windowManager.sway.enable = true;
+  wayland.windowManager.sway.config.assigns = {
+    ws2 = [{ class = "^Firefox$"; }];
+  };
+  wayland.windowManager.sway.config.input = {
+    "type:touchpad" = {
+      scroll_factor = "0.76";
+      natural_scroll = "enabled";
+      tap = "enabled";
+      tap_button_map = "lrm";
+      accel_profile = "adaptive";
+    };
+  };
+  wayland.windowManager.sway.config.fonts = {
+    names = [ "Iosevka Term" ];
+    style = "Bold";
+    size = 12.0;
+  };
+  wayland.windowManager.sway.config.workspaceAutoBackAndForth = true;
+  wayland.windowManager.sway.config.modifier = "Mod4";
+  wayland.windowManager.sway.config.keybindings =
+    let
+      mod = config.wayland.windowManager.sway.config.modifier;
+    in
+    lib.mkOptionDefault {
+      "${mod}+Return" = "exec ${pkgs.alacritty}/bin/alacritty";
+      "${mod}+Shift+q" = "kill";
+      "${mod}+d" = "exec ${pkgs.bemenu}/bin/bemenu-run | ${pkgs.findutils}/bin/xargs ${pkgs.sway}/bin/swaymsg exec --";
+      "${mod}+Shift+r" = "reload";
+      "${mod}+Shift+e" = "exec swaynag -t warning -m 'You pressed the exit shortcut. Do you really want to exit sway? This will end your Wayland session.' -b 'Yes, exit sway' 'swaymsg exit'";
+
+      # Brightness
+      "XF86MonBrightnessDown" = "exec light -U 10";
+      "XF86MonBrightnessUp" = "exec light -A 10";
+
+      # Volume
+      "XF86AudioRaiseVolume" = "exec 'pactl set-sink-volume @DEFAULT_SINK@ +1%'";
+      "XF86AudioLowerVolume" = "exec 'pactl set-sink-volume @DEFAULT_SINK@ -1%'";
+      "XF86AudioMute" = "exec 'pactl set-sink-mute @DEFAULT_SINK@ toggle'";
+
+      # Moving windows/focus around
+      "${mod}+Left" = "focus left";
+      "${mod}+Shift+Left" = "move left";
+      "${mod}+Right" = "focus right";
+      "${mod}+Shift+Right" = "move right";
+      "${mod}+Up" = "focus up";
+      "${mod}+Shift+Up" = "move up";
+      "${mod}+Down" = "focus down";
+      "${mod}+Shift+Down" = "move down";
+
+      # Move between workspaces
+      "${mod}+1" = "workspace ${ws1}";
+      "${mod}+Shift+1" = "move container to workspace ${ws1}";
+      "${mod}+2" = "workspace ${ws2}";
+      "${mod}+Shift+2" = "move container to workspace ${ws2}";
+      "${mod}+3" = "workspace ${ws3}";
+      "${mod}+Shift+3" = "move container to workspace ${ws3}";
+      "${mod}+4" = "workspace ${ws4}";
+      "${mod}+Shift+4" = "move container to workspace ${ws4}";
+      "${mod}+5" = "workspace ${ws5}";
+      "${mod}+Shift+5" = "move container to workspace ${ws5}";
+      "${mod}+6" = "workspace 6";
+      "${mod}+Shift+6" = "move container to workspace 6";
+      "${mod}+7" = "workspace 7";
+      "${mod}+Shift+7" = "move container to workspace 7";
+      "${mod}+8" = "workspace 8";
+      "${mod}+Shift+8" = "move container to workspace 8";
+      "${mod}+9" = "workspace 9";
+      "${mod}+Shift+9" = "move container to workspace 9";
+      "${mod}+0" = "workspace 10";
+      "${mod}+Shift+0" = "move container to workspace 10";
+
+      # Move between displays
+      "${mod}+Ctrl+Shift+Right" = "move workspace to output right";
+      "${mod}+Ctrl+Shift+Left" = "move workspace to output left";
+      "${mod}+Ctrl+Shift+Down" = "move workspace to output down";
+      "${mod}+Ctrl+Shift+Up" = "move workspace to output up";
+
+      # Layout manipulation
+      "${mod}+h" = "splith";
+      "${mod}+v" = "splitv";
+      "${mod}+s" = "layout stacking";
+      "${mod}+w" = "layout tabbed";
+      "${mod}+e" = "layout toggle split";
+      "${mod}+f" = "fullscreen";
+      "${mod}+Shift+space" = "floating toggle";
+      "${mod}+p" = "focus parent";
+    };
+  wayland.windowManager.sway.config.startup = [
+    { command = "dbus-sway-environment"; }
+    { command = "configure-gtk"; }
+  ];
+  # configure colors and bars in extraConfig for convenient copy/paste from
+  # base16 templates
+  wayland.windowManager.sway.config.bars = [ ];
+  wayland.windowManager.sway.extraConfig = ''
+    # Rose Pine
+    set $base00 #191724
+    set $base01 #1f1d2e
+    set $base02 #26233a
+    set $base03 #555169
+    set $base04 #6e6a86
+    set $base05 #e0def4
+    set $base06 #f0f0f3
+    set $base07 #c5c3ce
+    set $base08 #eb6f92
+    set $base09 #f6c177
+    set $base0A #f6c177
+    set $base0B #31748f
+    set $base0C #ebbcba
+    set $base0D #9ccfd8
+    set $base0E #c4a7e7
+    set $base0F #e5e5e5
+
+    bar {
+      colors {
+        background $base00
+        separator  $base01
+        statusline $base04
+
+        #                   Border  BG      Text
+        focused_workspace   $base00 $base0D $base00
+        active_workspace    $base00 $base03 $base00
+        inactive_workspace  $base00 $base01 $base05
+        urgent_workspace    $base08 $base08 $base00
+        binding_mode        $base00 $base0A $base00
+      }
+    }
+
+    #                       Border  BG      Text    Ind     Child Border
+    client.focused          $base05 $base0D $base00 $base0D $base0D
+    client.focused_inactive $base01 $base01 $base05 $base03 $base01
+    client.unfocused        $base01 $base00 $base05 $base01 $base01
+    client.urgent           $base08 $base08 $base00 $base08 $base08
+
+    output * bg $base00 solid_color
+  '';
+
+  # TODO: verify this has sane defaults
+  services.swayidle.enable = true;
+
+  services.kanshi = {
+    enable = true;
+    profiles = {
+      undocked = {
+        outputs = [
+          {
+            criteria = "eDP-1";
+            status = "enable";
+            scale = 1.5;
+          }
+        ];
+      };
+      docked-home = {
+        outputs = [
+          {
+            criteria = "Dell Inc. DELL U2717D 67YGV71AA15L";
+            status = "enable";
+            mode = "2560x1440@60Hz";
+            position = "0,0";
+          }
+          {
+            criteria = "Ancor Communications Inc ROG PG279Q #ASM6IrOLXMDd";
+            status = "enable";
+            mode = "2560x1440@60Hz"; # TODO: 165hz doesnt work :(
+            position = "2560,0";
+          }
+          {
+            criteria = "eDP-1";
+            status = "enable";
+            scale = 1.5;
+            position = "5120,0";
+          }
+        ];
+      };
+    };
   };
 
   # This value determines the Home Manager release that your
--- system.nix
+++ system.nix
@@ -1,5 +1,50 @@
 { config, pkgs, ... }:
 
+let
+  ## VIA https://nixos.wiki/wiki/Sway
+  # bash script to let dbus know about important env variables and
+  # propogate them to relevent services run at the end of sway config
+  # see
+  # https://github.com/emersion/xdg-desktop-portal-wlr/wiki/"It-doesn't-work"-Troubleshooting-Checklist
+  # note: this is pretty much the same as  /etc/sway/config.d/nixos.conf but
+  # also restarts  some user services to make sure they have the correct
+  # environment variables
+  dbus-sway-environment = pkgs.writeTextFile {
+    name = "dbus-sway-environment";
+    destination = "/bin/dbus-sway-environment";
+    executable = true;
+
+    text = ''
+      dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
+      systemctl --user stop pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
+      systemctl --user start pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
+    '';
+  };
+
+  ## VIA https://nixos.wiki/wiki/Sway
+  # currently, there is some friction between sway and gtk:
+  # https://github.com/swaywm/sway/wiki/GTK-3-settings-on-Wayland
+  # the suggested way to set gtk settings is with gsettings
+  # for gsettings to work, we need to tell it where the schemas are
+  # using the XDG_DATA_DIR environment variable
+  # run at the end of sway config
+  configure-gtk = pkgs.writeTextFile {
+    name = "configure-gtk";
+    destination = "/bin/configure-gtk";
+    executable = true;
+    text =
+      let
+        schema = pkgs.gsettings-desktop-schemas;
+        datadir = "${schema}/share/gsettings-schemas/${schema.name}";
+      in
+      ''
+        export XDG_DATA_DIRS=${datadir}:$XDG_DATA_DIRS
+        gnome_schema=org.gnome.desktop.interface
+        gsettings set $gnome_schema gtk-theme 'Rose-Pine'
+        gsettings set $gnome_schema cursor-theme 'Adwaita'
+      '';
+  };
+in
 {
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
@@ -18,22 +63,66 @@
   # Set your time zone.
   time.timeZone = "America/Toronto";
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  ## BEGIN Sway config
 
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  environment.systemPackages = with pkgs; [
+    wayland
+    sway
+    swayidle
+    swaylock
+    swaybg
+    wob # meters for brightness/volume
+    dbus-sway-environment
+    configure-gtk
+    glib # gsettings
+    alacritty
+    rose-pine-gtk-theme # gtk theme
+    gnome3.adwaita-icon-theme # default gnome cursors
+    wl-clipboard # wl-copy and wl-paste for copy/paste from stdin / stdout
+    bemenu # wayland clone of dmenu
+    mako # notification system developed by swaywm maintainer
+    pulseaudio # until pw-cli has commands for volume control
+  ];
+  environment.loginShellInit = ''
+    [[ "$(tty)" == /dev/tty1 ]] && sway
+  '';
+
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # xdg-desktop-portal works by exposing a series of D-Bus interfaces
+  # known as portals under a well-known name
+  # (org.freedesktop.portal.Desktop) and object path
+  # (/org/freedesktop/portal/desktop).
+  # The portal interfaces include APIs for file access, opening URIs,
+  # printing and others.
+  services.dbus.enable = true;
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    # gtk portal needed to make gtk apps happy
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    gtkUsePortal = true;
+  };
+
+  # enable sway window manager
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true;
+  };
+
+  # brightness control
+  programs.light.enable = true;
+
+  ## END Sway config
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
-
-  # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Enable fingerprint reader.
   services.fprintd.enable = true;
@@ -74,7 +163,7 @@
   # My user account.
   users.users.vito = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" "docker" ];
+    extraGroups = [ "wheel" "video" "networkmanager" "docker" ];
     shell = pkgs.fish;
   };
 
@@ -84,4 +173,3 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 }
-
