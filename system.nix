bab76a6ba1eddc32717ff83794cad5b2f31107cd
Alex Suraci <suraci.alex@gmail.com>
2022-09-05 12:09:10 -0400

    better font smoothing
    i was seeing pretty bad rgb hinting artifacts in some applications (e.g.
    mako); bringing back this config from ye olde dotfiles of yore fixes it

--- system.nix
+++ system.nix
@@ -155,6 +155,37 @@ in
     defaultFonts = {
       monospace = [ "Iosevka Term" ];
     };
+    localConf = ''
+      <?xml version='1.0'?>
+      <!DOCTYPE fontconfig SYSTEM 'urn:fontconfig:fonts.dtd'>
+      <fontconfig>
+        <match target="font">
+          <edit mode="assign" name="rgba">
+            <const>rgb</const>
+          </edit>
+        </match>
+        <match target="font">
+          <edit mode="assign" name="hinting">
+            <bool>true</bool>
+          </edit>
+        </match>
+        <match target="font">
+          <edit mode="assign" name="hintstyle">
+            <const>hintslight</const>
+          </edit>
+        </match>
+        <match target="font">
+          <edit mode="assign" name="antialias">
+            <bool>true</bool>
+          </edit>
+        </match>
+        <match target="font">
+          <edit mode="assign" name="lcdfilter">
+            <const>lcddefault</const>
+          </edit>
+        </match>
+      </fontconfig>
+    '';
   };
 
   # Enable Docker.
