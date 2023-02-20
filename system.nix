b832fdc111062a1f70281bedfb0f6426597bbd5b
Alex Suraci <suraci.alex@gmail.com>
2023-02-20 16:07:07 -0500

    install symbols nerd font and fallback config

--- system.nix
+++ system.nix
@@ -158,6 +158,37 @@ in
 
     font-awesome # for waybar
     corefonts # common Microsoft fonts
+
+    (stdenv.mkDerivation {
+      pname = "symbols-nerd-font";
+      version = "2.2.0";
+      src = fetchFromGitHub {
+        owner = "ryanoasis";
+        repo = "nerd-fonts";
+        rev = "bde5c7def1775de187aae810e08d28a804e676da";
+        sha256 = "nvqtakqdhzmBbycNGjSOki+my1/58PozDzJa9jcIvtU=";
+        sparseCheckout = ''
+          10-nerd-font-symbols.conf
+          patched-fonts/NerdFontsSymbolsOnly
+        '';
+      };
+      dontConfigure = true;
+      dontBuild = true;
+      installPhase = ''
+        runHook preInstall
+
+        fontconfigdir="$out/etc/fonts/conf.d"
+        install -d "$fontconfigdir"
+        install 10-nerd-font-symbols.conf "$fontconfigdir"
+
+        fontdir="$out/share/fonts/truetype"
+        install -d "$fontdir"
+        install "patched-fonts/NerdFontsSymbolsOnly/complete/Symbols-2048-em Nerd Font Complete.ttf" "$fontdir"
+
+        runHook postInstall
+      '';
+      enableParallelBuilding = true;
+    })
   ];
   fonts.enableDefaultFonts = true;
   fonts.fontconfig = {
