85737ec1bed4d71a578e894828a5457e30d34a16
Alex Suraci <suraci.alex@gmail.com>
2023-04-13 13:50:19 -0400

    compat

--- home.nix
+++ home.nix
@@ -195,7 +195,7 @@ in
         {
           extraGrammars = {
             tree-sitter-bass = {
-              src = _: ~/src/tree-sitter-bass;
+              src = ~/src/tree-sitter-bass;
             };
           };
         };
