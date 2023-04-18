6fb19034064030c16a247f4d3aa62d6c9fcb64e5
Alex Suraci <suraci.alex@gmail.com>
2023-04-18 12:16:44 -0400

    fmt

--- system.nix
+++ system.nix
@@ -78,7 +78,7 @@ in
     enable = true;
     extraPackages = with pkgs; [
       intel-media-driver # LIBVA_DRIVER_NAME=iHD
-      vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
       vaapiVdpau
       libvdpau-va-gl
     ];
