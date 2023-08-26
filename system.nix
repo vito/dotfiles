23a7b22ac26c84ddb4ff51842359bc3aea4fde76
Alex Suraci <suraci.alex@gmail.com>
2023-08-26 13:19:45 -0400

    remove accelerated video attempt
    this was breaking nixos-rebuild switch, and I'm pretty sure it never
    worked anyway

--- system.nix
+++ system.nix
@@ -72,20 +72,6 @@ in
   # Enable updating firmware.
   services.fwupd.enable = true;
 
-  # Enable accelerated video playback.
-  nixpkgs.config.packageOverrides = pkgs: {
-    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
-  };
-  hardware.opengl = {
-    enable = true;
-    extraPackages = with pkgs; [
-      intel-media-driver # LIBVA_DRIVER_NAME=iHD
-      vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
-      vaapiVdpau
-      libvdpau-va-gl
-    ];
-  };
-
   ## BEGIN Sway config
 
   environment.systemPackages = with pkgs; [
