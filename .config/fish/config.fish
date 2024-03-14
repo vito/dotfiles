40d8fdb3de770777f99495550a495a3ec5ce8681
Alex Suraci <suraci.alex@gmail.com>
2024-03-13 21:26:03 -0400

    add host
    Signed-off-by: Alex Suraci <suraci.alex@gmail.com>

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,6 +7,8 @@ set -x EDITOR vim
 alias gst 'git status'
 alias gs 'git status'
 
+alias host 'distrobox-host-exec'
+
 if status is-interactive
   starship init fish | source
   fish_config theme choose rose-pine
