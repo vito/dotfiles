5e298b7bf92c6b739f43f8e42efd5854adf078b9
Alex Suraci <suraci.alex@gmail.com>
2024-03-29 13:23:16 -0400

    add pnpm config
    Signed-off-by: Alex Suraci <suraci.alex@gmail.com>

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,3 +14,10 @@ if status is-interactive
   starship init fish | source
   fish_config theme choose rose-pine
 end
+
+# pnpm
+set -gx PNPM_HOME "/home/vito/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
